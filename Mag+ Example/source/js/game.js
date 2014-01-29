ba6275d6d848e4732d5b9fa7d38839ef05ed62ed
bryanerayner <bryanerayner@email.com>
2014-01-29 16:19:01 -0500

    Add Magplus stuff

--- Mag+ Example/source/js/game.js
+++ Mag+ Example/source/js/game.js
@@ -5,7 +5,7 @@ $(document).ready(function()
 	game = new MadLibs.Page($(".page.page-input"), $(".page.page-results"));
 
 
-	$("#btn-seeResults").on("click", function(){
+	$(".btn.btn-seeList").on("click", function(){
 		if (game.viewPage("results"))
 		{
 			// Response for succesfully viewing the page
@@ -14,17 +14,6 @@ $(document).ready(function()
 		else
 		{
 			alert("You need to fill out all the inputs!");
-		}
-	});
-	$("#btn-seeList").on("click", function(){
-		if (game.viewPage("input"))
-		{
-			// Response for succesfully viewing the page
-
-		}
-		else
-		{
-			
 		}
 	});
 
