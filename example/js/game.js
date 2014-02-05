b69b60b66214551fdf3dcd93ffea56db522e7d6c
bryanerayner <bryanerayner@end.com>
2014-02-05 15:48:46 -0500

    Make Example

---
+++ example/js/game.js
@@ -0,0 +1,59 @@
+var game;
+
+$(document).ready(function()
+{
+	game = new MadLibs.Page($(".page.page-input"), $(".page.page-results"));
+	
+	// This code switches the game to the popup and then quickly back to the input. This rectifies a problem with the game on Android tablets.
+	_.defer(function(){
+		game.viewPage("intro");
+		_.defer(function(){
+			game.viewPage("input");			
+		});	
+	});
+
+	$("#btn-ok").on("click", function(){
+		if (game.viewPage("input"))
+		{
+			// Response for succesfully viewing the page
+		}
+		else
+		{
+			game.viewPage("input");
+		}
+	});
+	$("#btn-seeResults").on("click", function(){
+		if (game.viewPage("results"))
+		{
+			// Response for succesfully viewing the page
+
+		}
+		else
+		{
+			game.viewPage("popup");
+		}
+	});
+	$("#btn-seeList").on("click", function(){
+		if (game.viewPage("input"))
+		{
+			// Response for succesfully viewing the page
+			game.reset();
+		}
+		else
+		{
+			
+		}
+	});
+
+
+	/*
+	
+	Should you wish to display another page here, use code similar to the following:
+
+
+	game.viewPage("myPageNameHere");
+
+	 */
+
+
+});
